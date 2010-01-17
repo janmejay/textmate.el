72ad58f2293f103f2a34554920da7e8f25a98294
Janmejay Singh <singh.janmejay@gmail.com>
2010-01-17 11:45:36 -0800

    ignoring vendor confused me, so unignoring vendor

--- textmate.el
+++ textmate.el
@@ -65,7 +65,7 @@
 ;;; Minor mode
 
 (defvar *textmate-gf-exclude*
-  "/\\.|vendor|fixtures|tmp|log|build|\\.xcodeproj|\\.nib|\\.framework|\\.app|\\.pbproj|\\.pbxproj|\\.xcode|\\.xcodeproj|\\.bundle|\\.pyc|\\.elc|\\.o|\\.so"
+  "/\\.|fixtures|tmp|log|build|\\.xcodeproj|\\.nib|\\.framework|\\.app|\\.pbproj|\\.pbxproj|\\.xcode|\\.xcodeproj|\\.bundle|\\.pyc|\\.elc|\\.o|\\.so"
   "Regexp of files to exclude from `textmate-goto-file'.")
 
 (defvar *textmate-project-roots*
